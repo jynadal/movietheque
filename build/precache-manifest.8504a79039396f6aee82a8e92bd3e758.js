92bb7e6b68c03823f871ef32493a36d5318c2206
jynadal <nadaljeanyves@hotmail.fr>
2020-09-28 21:24:16 +0200

    Finaly i'm  deploying now

---
+++ build/precache-manifest.8504a79039396f6aee82a8e92bd3e758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e986fffca16091d1af4b8ccb513c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e26c9766636995d34de",
+    "url": "/static/css/main.ae0dc5b2.chunk.css"
+  },
+  {
+    "revision": "e0c449ef1c84865b824b",
+    "url": "/static/js/2.2cc0fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2cc0fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e26c9766636995d34de",
+    "url": "/static/js/main.4498f258.chunk.js"
+  },
+  {
+    "revision": "fa72b966919c8438d3f1",
+    "url": "/static/js/runtime-main.9e3bc866.js"
+  }
+]);
